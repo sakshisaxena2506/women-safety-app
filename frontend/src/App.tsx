106d37fc92c4b84932f0baa5cad5d741981a47a0
Sakshi Saxena <sakshisaxena2506@gmail.com>
2026-05-12 23:43:12 +0530

    fixed landing import path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 // Public pages
-import Landing from './pages/Landing';
+import Landing from './pages/landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
